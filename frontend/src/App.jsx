99f0deb08afbb1260ac5d6280efa6514ca1bb8a5
javed khan <javedofficial221@gmail.com>
2026-03-05 11:25:08 +0530

    add mongodb atlas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,68 +17,68 @@ import ProtectedRoute from './components/admin/ProtectedRoute'
 
 
 const appRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/signup',
-    element: <Signup />
-  },
-  {
-    path: "/jobs",
-    element: <Jobs />
-  },
-  {
-    path: "/description/:id",
-    element: <JobDescription />
-  },
-  {
-    path: "/browse",
-    element: <Browse />
-  },
-  {
-    path: "/profile",
-    element: <Profile />
-  },
-  // admin ke liye yha se start hoga
-  {
-    path:"/admin/companies",
-    element: <ProtectedRoute><Companies/></ProtectedRoute>
-  },
-  {
-    path:"/admin/companies/create",
-    element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/companies/:id",
-    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs",
-    element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs/create",
-    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs/:id/applicants",
-    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
-  },
+	{
+		path: '/',
+		element: <Home />
+	},
+	{
+		path: '/login',
+		element: <Login />
+	},
+	{
+		path: '/signup',
+		element: <Signup />
+	},
+	{
+		path: "/jobs",
+		element: <Jobs />
+	},
+	{
+		path: "/description/:id",
+		element: <JobDescription />
+	},
+	{
+		path: "/browse",
+		element: <Browse />
+	},
+	{
+		path: "/profile",
+		element: <Profile />
+	},
+	// admin ke liye yha se start hoga
+	{
+		path: "/admin/companies",
+		element: <ProtectedRoute><Companies /></ProtectedRoute>
+	},
+	{
+		path: "/admin/companies/create",
+		element: <ProtectedRoute><CompanyCreate /></ProtectedRoute>
+	},
+	{
+		path: "/admin/companies/:id",
+		element: <ProtectedRoute><CompanySetup /></ProtectedRoute>
+	},
+	{
+		path: "/admin/jobs",
+		element: <ProtectedRoute><AdminJobs /></ProtectedRoute>
+	},
+	{
+		path: "/admin/jobs/create",
+		element: <ProtectedRoute><PostJob /></ProtectedRoute>
+	},
+	{
+		path: "/admin/jobs/:id/applicants",
+		element: <ProtectedRoute><Applicants /></ProtectedRoute>
+	},
 
 ])
 function App() {
 
-  return (
-    <div>
-      <RouterProvider router={appRouter} />
-    </div>
-  )
+	return (
+		<div>
+			<RouterProvider router={appRouter} />
+		</div>
+	)
 }
 
 export default App
