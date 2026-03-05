3eb85572af3a618e9ebac596ce3abee614ce4f4a
javed khan <javedofficial221@gmail.com>
2026-03-05 11:59:05 +0530

    add frontend url

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,19 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// const corsOptions = {
+//     origin:'http://localhost:5173',
+//     credentials:true
+// }
+
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
+	origin: [
+		"http://localhost:5173",
+		"https://jobhub-g1x0.onrender.com"
+	],
+	credentials: true
 }
 
 app.use(cors(corsOptions));
@@ -34,7 +42,7 @@ app.use("/api/v1/application", applicationRoute);
 
 
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
+app.listen(PORT, () => {
+	connectDB();
+	console.log(`Server running at port ${PORT}`);
 })
